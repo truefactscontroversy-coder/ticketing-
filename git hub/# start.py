ded81393ac1b195a400d8e7c8fd22b4dac9141eb
truefactscontroversy-coder <truefactscontroversy@gmail.com>
2025-11-22 10:52:28 +0000

    added departure zone

--- git hub/# start.py
+++ git hub/# start.py
@@ -11,3 +11,28 @@
  
  print("new page loading") 
 # need to have a way to send code back to start
+# add departure 
+def printed_zones ():
+  zones = ["Down town zone", "Mid town zone", "Central zone"]
+  print(zones) 
+printed_zones()
+print("Please select a starting zone: ")
+
+your_location = input()
+
+select_zone = your_location
+
+ while select_zone == "":
+   printed_zones()
+   print("Please select a zone") 
+   select_zone = input()
+   if select_zone != "":
+    your_location = select_zone
+    voucher.append (F"Departure zone: {your_location}")
+    print("New page loading")
+    break
+   else: 
+    voucher.append (F"Departure zone: {your_location}")
+    print("New page loading")
+   
+ 
