9df8d278ac46f05736cee6885e8f6153035dbdef
truefactscontroversy-coder <truefactscontroversy@gmail.com>
2025-11-22 11:36:53 +0000

    added restart feature by turning main code into a function

--- git hub/# start.py
+++ git hub/# start.py
@@ -1,4 +1,5 @@
-# start 
+def ticketing_system():
+ # start 
  station_board = ("board + your location")
  start_now = input ("start voucher ")
 
@@ -9,18 +10,18 @@
    break 
  voucher = []
  
-print("new page loading") 
-# need to have a way to send code back to start
-# add departure 
-def printed_zones ():
+ print("new page loading") 
+ # need to have a way to send code back to start
+ # add departure 
+ def printed_zones ():
   zones = ["Down town zone", "Mid town zone", "Central zone"]
   print(zones) 
-printed_zones()
-print("Please select a starting zone: ")
+ printed_zones()
+ print("Please select a starting zone: ")
 
-your_location = input()
+ your_location = input()
 
-select_zone = your_location
+ select_zone = your_location
 
  while select_zone == "":
    printed_zones()
@@ -35,12 +36,12 @@ select_zone = your_location
     voucher.append (F"Departure zone: {your_location}")
     print("New page loading")
 
-# add destination  
-# problems not sure if the calculations are correct
-print("station board with all zones and stations")
-printed_zones()
-print("Please select a destinationn zone: ")
-destination_zone = input()
+ # add destination  
+ # problems not sure if the calculations are correct
+ print("station board with all zones and stations")
+ printed_zones()
+ print("Please select a destinationn zone: ")
+ destination_zone = input()
 
  
 
@@ -53,35 +54,35 @@ destination_zone = input()
    
  
 
-if your_location == select_zone:
+ if your_location == select_zone:
     zone_num = 1
     voucher.append (f"Destination zone: {destination_zone}")
     print("New page loading")
-elif your_location == "Down town zone" and destination_zone == "Central zone":
+ elif your_location == "Down town zone" and destination_zone == "Central zone":
     zone_num = 3
     voucher.append (f"Destination zone: {destination_zone}")
     print("New page loading")
-elif your_location == "Central zone" and destination_zone == "Down town zone":
+ elif your_location == "Central zone" and destination_zone == "Down town zone":
     zone_num = 3
     voucher.append (f"Destination zone: {destination_zone}")
     print("New page loading")
-elif your_location == "Mid town zone" :
+ elif your_location == "Mid town zone" :
      zone_num = 2 
      voucher.append (f"Destination zone: {destination_zone}")
      print("New page loading")
 
      
-# add destination station
+ # add destination station
 
-down_town = ("Adohad, Brunad, Edert, Elyot, Erean, Holmer, Kelvia, Mareng, Perinad, Pryn, Ruril, Ryall, Zord")
+ down_town = ("Adohad, Brunad, Edert, Elyot, Erean, Holmer, Kelvia, Mareng, Perinad, Pryn, Ruril, Ryall, Zord")
 
-mid_town = ("Agraile, Docia, Garon, Obelyn, Oloadus, Quthiel, Ralith, Riciva, Riladia, Sylas, Wicyt")
+ mid_town = ("Agraile, Docia, Garon, Obelyn, Oloadus, Quthiel, Ralith, Riciva, Riladia, Sylas, Wicyt")
 
-central = ("Centrala Frestin Jaund Lomil Ninia Rede Riciva Soth Sylyn Tallan")
+ central = ("Centrala Frestin Jaund Lomil Ninia Rede Riciva Soth Sylyn Tallan")
 
 
 
-if destination_zone == "Down town zone":
+ if destination_zone == "Down town zone":
   print(f"List of stations for down town: {down_town}")
  elif destination_zone == "Mid town zone":
   print(f"List of stations for midtown: {mid_town}")
@@ -89,8 +90,8 @@ if destination_zone == "Down town zone":
   print(f"List of stations for {central}")
 
 
-print("Please select a station: ")
-destination_station = input()
+ print("Please select a station: ")
+ destination_station = input()
 
  while destination_station == "":
   print("please select a station")
@@ -99,17 +100,17 @@ destination_station = input()
    print("New page loading")
    break
 
-voucher.append (destination_station)
-select_zone = destination_station
+ voucher.append (destination_station)
+ select_zone = destination_station
 
-# add passenger 
-# problems can't add user input to an already defined variable?
-print("price page")
-print("age selction and add buttons")
+ # add passenger 
+ # problems can't add user input to an already defined variable?
+ print("price page")
+ print("age selction and add buttons")
 
 
  
-print("Adult: -1 - +1 or none")
+ print("Adult: -1 - +1 or none")
  Adult = input()
  print("Child: -1 - +1 or none")
  Child = input()
@@ -118,7 +119,7 @@ print("Adult: -1 - +1 or none")
  print("Elderly: -1 - +1 or none")
  Elderly = input()
 
-#passenger calculation
+ #passenger calculation
  # for adult there is something in the variable if there is a 0 nned to figure this out!!!
  if adult_num != 0:
   zone_num *= 21.05 
@@ -176,9 +177,9 @@ print("Adult: -1 - +1 or none")
 
  # voucher print
  
-print(voucher)
+ print(voucher)
  
-#passenger calculation
+ #passenger calculation
  # for adult there is something in the variable if there is a 0 nned to figure this out!!!
  if adult_num != 0:
   zone_num *= 21.05 
@@ -237,14 +238,15 @@ print(voucher)
  # voucher print
  
 
-print(voucher)
+ print(voucher)
  
-# restart or end 
+ # restart or end 
  restart_end = input("Please select start a new voucher by selecting restart or end processs by selecting end process")
  while restart_end == "":
    restart_end = input("Please select start a new voucher or end processs")
  if restart_end == "end process":
   print("new page loading")
  elif restart_end == "restart": 
+   ticketing_system()
 
- 
+ticketing_system()
