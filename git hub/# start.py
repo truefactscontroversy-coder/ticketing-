dd4d44b8d204f1add15396f33959337ebb85d0cf
truefactscontroversy-coder <truefactscontroversy@gmail.com>
2025-11-22 10:59:55 +0000

    added destination unit

--- git hub/# start.py
+++ git hub/# start.py
@@ -34,5 +34,41 @@ select_zone = your_location
    else: 
     voucher.append (F"Departure zone: {your_location}")
     print("New page loading")
+
+# add destination  
+# problems not sure if the calculations are correct
+print("station board with all zones and stations")
+printed_zones()
+print("Please select a destinationn zone: ")
+destination_zone = input()
+
+ 
+
+ while destination_zone == "":
+   printed_zones()
+   print("Please select a zone") 
+   destination_zone = input()
+   if destination_zone != "" :
+    break
    
+ 
+
+if your_location == select_zone:
+    zone_num = 1
+    voucher.append (f"Destination zone: {destination_zone}")
+    print("New page loading")
+elif your_location == "Down town zone" and destination_zone == "Central zone":
+    zone_num = 3
+    voucher.append (f"Destination zone: {destination_zone}")
+    print("New page loading")
+elif your_location == "Central zone" and destination_zone == "Down town zone":
+    zone_num = 3
+    voucher.append (f"Destination zone: {destination_zone}")
+    print("New page loading")
+elif your_location == "Mid town zone" :
+     zone_num = 2 
+     voucher.append (f"Destination zone: {destination_zone}")
+     print("New page loading")
+     
+  
  
