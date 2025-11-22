0a1799a58ae1dd0730bede2273b2e5bb2b3f8e60
truefactscontroversy-coder <truefactscontroversy@gmail.com>
2025-11-22 10:19:38 +0000

    Add files via upload

---
+++ git hub/# start.py
@@ -0,0 +1,13 @@
+# start 
+ station_board = ("board + your location")
+ start_now = input ("start voucher ")
+
+ while start_now == "":
+  print("please select: start voucher")
+  start_now = input ("start voucher ")
+  if start_now == "start voucher":
+   break 
+ voucher = []
+ 
+ print("new page loading") 
+# need to have a way to send code back to start
