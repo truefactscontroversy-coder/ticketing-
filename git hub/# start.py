55bceefb49ee2be5695224e9ccf51e42c7c79bfb
truefactscontroversy-coder <truefactscontroversy@gmail.com>
2025-11-24 11:21:13 +0000

    add passenger unit testing commit

--- git hub/# start.py
+++ git hub/# start.py
@@ -234,11 +234,11 @@ def ticketing_system():
   total_passenger = Adult + Child + Student + Elderly
   return total_passenger
  
- passenger_catagory()
+ total_passenger = passenger_catagory()
 
- if (passenger_catagory() != 0) : 
+ if (total_passenger != 0) : 
   print("New page loading")
-  voucher.append (f"Total number of passengers: {passenger_catagory()}")
+  voucher.append (f"Total number of passengers: {total_passenger}")
  else:
    print("please add at least one passenger")
    while (passenger_catagory() == 0 ):
